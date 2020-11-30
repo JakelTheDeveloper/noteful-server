45c6c8e817804b9c9d98d3dd621815022497e925
JaKel Jenkins <jakelj24@gmail.com>
2020-11-30 11:24:25 -0500

    noteful server ready for deployment

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'start-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://jakel:hello@localhost/noteful_dbase',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jakel:hello@localhost/noteful_dbase',
 }
